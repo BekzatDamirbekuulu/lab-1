26257c3b5fd409195b96187cbdfd3084d06116f4
zikirovnurislam <zikirovnurislam@gmail.com>
2024-11-05 12:15:44 +0300

    lab 1 task 4 переход в вторую форму

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -5,15 +5,31 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
-            InitializeComponent();
-            this.FormClosing += Form1_FormClosing; 
+            Button exitButton = new Button();
+            exitButton.Text = "Выход";
+            exitButton.Size = new System.Drawing.Size(100, 50);
+            exitButton.Location = new System.Drawing.Point(50, 50);
+            exitButton.Click += ExitButton_Click;
+
+            Button transitionButton = new Button();
+            transitionButton.Text = "Переход";
+            transitionButton.Size = new System.Drawing.Size(100, 50);
+            transitionButton.Location = new System.Drawing.Point(50, 150);
+            transitionButton.Click += TransitionButton_Click;
+
+            this.Controls.Add(exitButton);
+            this.Controls.Add(transitionButton);
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void TransitionButton_Click(object sender, EventArgs e)
         {
             Form2 form2 = new Form2();
-            form2.ShowDialog(); 
-            this.Dispose();
+            form2.Show();
         }
+
     }
 }
--- WinFormsApp1/Form2.cs
+++ WinFormsApp1/Form2.cs
@@ -15,6 +15,31 @@ namespace WinFormsApp1
         public Form2()
         {
             InitializeComponent();
+            Button exitButton = new Button();
+            exitButton.Text = "Выход";
+            exitButton.Size = new System.Drawing.Size(100, 50);
+            exitButton.Location = new System.Drawing.Point(50, 50);
+            exitButton.Click += ExitButton_Click;
+
+            Button closeButton = new Button();
+            closeButton.Text = "Закрыть 2 форму";
+            closeButton.Size = new System.Drawing.Size(100, 50);
+            closeButton.Location = new System.Drawing.Point(50, 150);
+            closeButton.Click += CloseButton_Click;
+
+            this.Controls.Add(exitButton);
+            this.Controls.Add(closeButton);
+        }
+
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
         }
     }
 }
