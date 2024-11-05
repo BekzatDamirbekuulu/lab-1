942b33cd6ff2681b66d678345b0285d2a9a575a5
zikirovnurislam <zikirovnurislam@gmail.com>
2024-11-05 11:55:02 +0300

    exit button

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -5,7 +5,21 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
-            
+            Button exitButton = new Button();
+            exitButton.Text = "Выход";
+            exitButton.Size = new System.Drawing.Size(100, 50);
+            exitButton.Location = new System.Drawing.Point(100, 100);
+
+            // Обработчик события нажатия на кнопку
+            exitButton.Click += new EventHandler(ExitButton_Click);
+
+            // Добавление кнопки на форму
+            this.Controls.Add(exitButton);
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            // Закрытие формы
+            this.Close();
         }
     }
 }
