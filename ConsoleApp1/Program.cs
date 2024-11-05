07b17cb8c4d29830fbad664b521895164133ebb4
zikirovnurislam <zikirovnurislam@gmail.com>
2024-11-05 12:38:01 +0300

    console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+        Console.Write("Введите первое целое число: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе целое число: ");
+        int secondNumber = int.Parse(Console.ReadLine()); 
+        int sum = firstNumber + secondNumber;
+
+        Console.WriteLine("Сумма чисел: " + sum);
+
+        Console.ReadKey();
